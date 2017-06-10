35b5fd2d2007ffe45cf1a59113e8547a6a56ad40
halwu(吴浩麟) <halwu@tencent.com>
2017-06-10 14:19:33 +0800

    ref: use koa-chrome-render

--- README.md
+++ README.md
@@ -39,15 +39,6 @@ app.listen(3000);
 you can download and run this [complete demo](./demo/main.js)
 
 ## Options
+- `render` options from [koa-chrome-render](https://github.com/gwuhaolin/koa-chrome-render#render-options)
+- `cache` options from [koa-chrome-render](https://github.com/gwuhaolin/koa-chrome-render#cache-options)
 
-### chrome-render options
-- `maxTab`: `number` max tab chrome will open to render pages, default is no limit, `maxTab` used to avoid open to many tab lead to chrome crash. `ChromeRender` will create a tab poll to reuse tab for performance improve and resource reduce as open and close tab in chrome require time, like database connection poll. 
-- `renderTimeout`: `number` in ms, `chromeRender.render()` will throw error if html string can't be resolved after `renderTimeout`, default is 5000ms.
-- `ready`: `string` is an option param. if it's omitted chrome will return page html on dom event `domContentEventFired`, else will waiting util js in web page call `console.log(${ready's value})`. et `ready=_ready_flag` when web page is ready call `console.log('_ready_flag')`.
-- `script`: `string` is an option param. inject script source to evaluate when page on load
-
-### redis cache options
-options `cache` is not required, if it's omitted will not use cache.
-`cache` is a object, is support all params in [node redis driver](https://github.com/NodeRedis/node_redis#options-object-properties),
-and below:
-- `expires`: `number` cache expires time in seconds, if omitted cache forever
--- index.js
+++ index.js
@@ -1,81 +1,56 @@
 'use strict';
-const ChromeRender = require('chrome-render');
-const { isCrawler } = require('./util');
-const Cacher = require('./cacher');
+const chromeRenderMiddleware = require('koa-chrome-render');
 
-function seoMiddleware(options = {
-    render: {
-        // number max tab chrome will open to render pages, default is no limit, maxTab used to avoid open to many tab lead to chrome crash.
-        maxTab: undefined,
-        // number in ms, chromeRender.render() will throw error if html string can't be resolved after renderTimeout, default is 5000ms.
-        renderTimeout: undefined,
+const CrawlerUserAgents = [
+    'googlebot',
+    'yahoo',
+    'bingbot',
+    'baiduspider',
+    'facebookexternalhit',
+    'twitterbot',
+    'rogerbot',
+    'linkedinbot',
+    'embedly',
+    'quora link preview',
+    'showyoubot',
+    'outbrain',
+    'pinterest/0.',
+    'developers.google.com/+/web/snippet',
+    'slackbot',
+    'vkShare',
+    'W3C_Validator',
+    'redditbot',
+    'Applebot',
+    'WhatsApp',
+    'flipboard',
+    'tumblr',
+    'bitlybot',
+    'SkypeUriPreview',
+    'nuzzel',
+    'Discordbot',
+    'Google Page Speed',
+    'Qwantify'
+]
 
-        // string is an option param. if it's omitted chrome will return page html on dom event domContentEventFired, else will waiting util js in web page call console.log(${ready's value}). et ready=_ready_flag when web page is ready call console.log('_ready_flag')
-        ready: undefined,
-        // string is an option param. inject script source to evaluate when page on load
-        script: undefined,
-    },
-    cache: undefined,
-}) {
-    const { render: chromeRenderOptions, cache: cacheOptions } = options;
-
-    let cacher;
-    if (cacheOptions !== undefined) {
-        cacher = new Cacher(cacheOptions);
-    }
-
-    let chromeRender;
-    (async () => {
-        chromeRender = await ChromeRender.new(chromeRenderOptions);
-    })();
-
-    // use chrome-render render page to html string
-    const render = async function (request) {
-        const { href, header } = request;
-        const referrer = header['referrer'];
-        const cookieString = header['cookies'];
-        let cookies;
-        if (typeof cookieString === 'string') {
-            cookies = {};
-            try {
-                cookieString.split('; ').forEach(em => {
-                    const two = em.split('=');
-                    cookies[two[0]] = cookies[two[1]];
-                })
-            } catch (_) {
-            }
-        }
-        return await chromeRender.render(Object.assign({
-            url: href,
-            referrer,
-            cookies,
-        }, chromeRenderOptions));
-    }
-
-    return async function (ctx, next) {
-        const { request } = ctx;
-        const { header, href } = request;
-        const ua = header['user-agent'];
-        const is = isCrawler(ua);
-        if (is) {
-            // is a bot req
-            let htmlString
-            if (cacher === undefined) {
-                // don't use cache
-                htmlString = await render(request);
-            } else {
-                // use cache
-                htmlString = await cacher.get(href);
-                if (htmlString === null) {
-                    htmlString = await render(request);
-                    cacher.set(href, htmlString);
-                }
-            }
-            ctx.body = htmlString;
-        } else {
-            await next();
+function isCrawler(request) {
+    const userAgent = request.headers['user-agent'];
+    for (let i = 0; i < CrawlerUserAgents.length; i++) {
+        const keyword = CrawlerUserAgents[i];
+        if (userAgent.indexOf(keyword) >= 0) {
+            return true;
         }
     }
+    return false;
+}
+
+function seoMiddleware(options = {}) {
+    const { render, cache } = options;
+
+    return chromeRenderMiddleware({
+        render,
+        cache,
+        enable: isCrawler
+    })
 }
 
 module.exports = seoMiddleware;
